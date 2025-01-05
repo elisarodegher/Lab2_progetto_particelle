d227b71f3b7e99e2ad5b1afaef46835096f7b17d
emmarubbi <peremmina@gmail.com>
2025-01-05 21:06:55 +0100

    test finiti

--- test/test_2.cxx
+++ test/test_2.cxx
@@ -2,5 +2,16 @@
 #include<array>
 
 int main() {
-    std::array<ParticleType*, 2> point_array;
+    
+    char n_1 = 'a';
+    char n_2 = 'b';
+    char* n_1_p{&n_1};
+    char* n_2_p{&n_2};
+    ParticleType part{n_1_p, 4.5, -1};
+    ResonanceType res{n_2_p, 3.5, 1, 0.5};
+    std::array<ParticleType*, 2> point_array {&part, &res};
+
+    for (int i = 0; i < 2; ++i) {
+        point_array[i]->Print();
+    }
 }
