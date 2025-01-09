1f97d1e6c15e7415ab59d15f80c1536c0adb44e5
artanshala <shala.artan@outlook.com>
2025-01-09 14:47:22 +0100

    Update Root_Analysis.cxx

--- source/Root_Analysis.cxx
+++ source/Root_Analysis.cxx
@@ -13,7 +13,14 @@ void ReadMyRootData()
     if (!hparticletypes) {
     std::cerr << "Errore: impossibile trovare l'istogramma hparticletypes!" << std::endl;
     return;
-}
+} 
+    //(*)(*) Artan
+    TCanvas *c0 = new TCanvas("c1","c1",900,600); // Qui ho inizializzato il primo canvas con i 4 istogrammi (numero di particelle, angolo theta, angolo phi, impulso), Artan
+    c0->Divide(2,2);
+    c0->cd(1);
+    hparticletypes->Draw();
+
+    
     int N_tot_conteggi = hparticletypes->GetEntries();
 
     if (N_tot_conteggi < 1E7 || N_tot_conteggi > 1.1E7)
@@ -48,6 +55,30 @@ void ReadMyRootData()
 
     htheta->Fit("FitTheta");
     hphi->Fit("FitPhi");
+    //(*)(*) Artan
+    c0->cd(2);
+    htheta->GetXaxis()->SetTitle("Angolo theta");
+    htheta->GetYaxis()->SetTitle("Occorrenze");
+    htheta->Draw();
+    htheta->Draw("SAME");
+    //(*)(*) Artan
+    c0->cd(3);
+    hphi->GetXaxis()->SetTitle("Angolo phi");
+    hphi->GetYaxis()->SetTitle("Occorrenze");
+    hphi->Draw();
+    hphi->Draw("SAME");
+
+    
+    // Le altre funzioni (*) le lasciate nel caso le funzioni Divide() e cd() non funzionino
+
+   //(*) TCanvas *htheta_Canvas = new TCanvas("htheta_Canvas","Distribuzione degli angoli Theta",900,600);
+   // htheta->Draw();
+
+   //(*) TCanvas *hphi_Canvas = new TCanvas("hphi_Canvas","Distribuzione degli angoli Phi",900,600);
+   // hphi->Draw();
+    // Artan
+
+    
 
     // a GetParameter() penso vada messo per forza un argomento in input, forse è il parametro 0 ma non sono sicura (da eli)
 
@@ -65,6 +96,16 @@ void ReadMyRootData()
     TF1 *FitImpulse = new TF1("FitImpulse", "expo(0)", 0., 6.);
 
     himpulse->Fit("FitImpulse");
+    //(*)(*) Artan
+    c0->cd(4);
+    himpulse->GetXaxis()->SetTitle("Impulso");
+    himpulse->GetYaxis()->SetTitle("Occorrenze");
+    himpulse->Draw();
+    himpulse->Draw("SAME");
+
+    // (*)TCanvas *himpulse_Canvas = new TCanvas("himpulse_Canvas","Distribuzione degli impulsi",900,600);
+    // himpulse->Draw();
+    //Artan
 
     std::cout << "Parametri dell'impulso: " << FitImpulse->GetParameter(0) << " +/- " << FitImpulse->GetParError(0) << std::endl
               << "Chi quadro ridotto: " << (FitImpulse->GetChisquare()/FitImpulse->GetNDF()) << std::endl
@@ -76,6 +117,9 @@ void ReadMyRootData()
 void AnalyseInvMass()
 {
     TFile *FileData = new TFile("Histograms.root");
+    //(*)(*) Artan
+    TCanvas *c1 = new TCanvas("c1","c1" 900,600);
+    c1->Divide(2,1);
 
     TH1F *h_samecharge_invmass = (TH1F *)FileData->Get("Invariant Mass of particles with concordant charge sign");
     TH1F *h_diffcharge_invmass = (TH1F *)FileData->Get("Invariant Mass of particles with discordant charge sign");
@@ -95,6 +139,24 @@ void AnalyseInvMass()
     h_invmass_difference->Fit("FitInvMass");
     h_pk_difference->Fit("FitPkInvMass");
     h_decayed_invmass->Fit("FitDecayedInvMass");
+    //(*)(*) Artan
+    c1->cd(1);
+    h_invmass_difference->GetXaxis()->SetTitle("Differenza di massa invariante tra particelle di stesso segno e particelle di segno discorde");
+    h_invmass_difference->GetYaxis()->SetTitle("Occorrenze");
+    h_invmass_difference->Draw();
+    h_invmass_difference->Draw("SAME");
+    //(*)(*) Artan
+    c1->cd(2);
+    h_pk_difference->GetXaxis()->SetTitle("Differenza di massa invariante tra pioni e kaoni di stesso segno e pioni e kaoni di segno discorde");
+    h_pk_difference->GetYaxis()->SetTitle("Occorrenze");
+    h_pk_difference->Draw();
+    h_pk_difference->Draw("SAME");
+    //(*)(*) Artan
+    c1->cd(3);
+    h_decayed_invmass->GetXaxis()->SetTitle("Massa invariante delle K* decadute");
+    h_decayed_invmass->GetYaxis()->SetTitle("Occorrenze");
+    h_decayed_invmass->Draw();
+    h_decayed_invmass->Draw("SAME");
 
     std::cout << "Invariant mass difference data: " << std::endl;
     std::cout << "K* mass(mean) = " << FitInvMass->GetParameter(1) << " +/- " << FitInvMass->GetParError(1) << std::endl;
@@ -113,6 +175,18 @@ void AnalyseInvMass()
     std::cout << "K* lenght(sigma) = " << FitDecayedInvMass->GetParameter(2) << " +/- " << FitDecayedInvMass->GetParError(2) << std::endl;
     std::cout << "Chisquare = " << (FitDecayedInvMass->GetChisquare() / FitDecayedInvMass->GetNDF())<< std::endl;
     std::cout << "Probability of chisquare = " << FitDecayedInvMass->GetProb() << std::endl;
+    //(*)(*) Artan
+    InvMassCanvas->SaveAs("InvMassCanvas.pdf");
+    PkInvMassCanvas->SaveAs("PkInvMassCanvas.pdf");
+    DecInvMassCanvas->SaveAs("DecInvMassCanvas.pdf");
+
+    InvMassCanvas->SaveAs("InvMassCanvas.root");
+    PkInvMassCanvas->SaveAs("PkInvMassCanvas.root");
+    DecInvMassCanvas->SaveAs("DecInvMassCanvas.root");
+
+    InvMassCanvas->SaveAs("InvMassCanvas.C");
+    PkInvMassCanvas->SaveAs("PkInvMassCanvas.C");
+    DecInvMassCanvas->SaveAs("DecInvMassCanvas.C");
 
     FileData -> Close();
 }
@@ -129,6 +203,8 @@ void ShowInvMassDiagrams()
 
     TH1F *h_invmass_difference = new TH1F(*h_samecharge_invmass);
     h_invmass_difference->Add(h_diffcharge_invmass, -1);
+     
+    
     TH1F *h_pk_difference = new TH1F(*h_pk_samesign_invmass);
     h_pk_difference->Add(h_pk_diffsign_invmass, -1);
 
